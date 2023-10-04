d38b69c791fd8d723051588d643ed45f64496899
gopinath <gopinathkaiser@gmail.com>
2023-10-04 16:08:12 +0530

    Adding project

---
+++ src/test/java/com/Products/Products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Products.Products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
